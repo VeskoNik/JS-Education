2262cb73fcd1535f000174f33c6b82becc3a9678
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-02-12 13:09:30 +0200

    Delete JS-Basic/JS-Fundamentals directory

--- JS-Basic/JS-Fundamentals/mToN.js
+++
@@ -1,6 +0,0 @@
-function solve(maxNum,minNum){
-    for (let i = maxNum ; i >= minNum ; i--) {
-        console.log(i);
-    }
-}
-solve(6,2)
--- JS-Basic/JS-Fundamentals/nto1.js
+++
@@ -1,7 +0,0 @@
-function solve(number) {
-    for (let index = number; index > 0; index--) {
-       console.log(index);
-        
-    }
-}
-solve(5)
